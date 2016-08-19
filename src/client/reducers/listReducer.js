96fbfbcb75846f78687c745100e01c35c079cad4
Antoine BARBAROUX <antoine.barbaroux@redpelicans.com>
2016-08-19 14:17:12 +0200

    Finished transition from lodash to ramda

--- src/client/reducers/listReducer.js
+++ src/client/reducers/listReducer.js
@@ -4,7 +4,9 @@
 // import _ from 'lodash'
 import { ADD_LIST, DEL_LIST, GOT_LISTS } from '../actions/lists'
 import { ADD_TASK, INPUT_TASK } from '../actions/tasks'
-import { append, reject, map, assoc } from 'ramda'
+import { append, assoc, evolve, map, memoize, pipe, reject } from 'ramda'
+
+const toInt = memoize(x => parseInt(x, 10)) // memoizing because I can
 
 export default function lists(state = [], action) {
   switch (action.type) {
@@ -13,23 +15,24 @@ export default function lists(state = [], action) {
 
   case DEL_LIST:
     return reject(list => list.id === action.id, state)
-    // return _.reject(state, list => (
-    //  list.id === parseInt(action.id)));
+    // -> native es6 features look prettier than Ramda here
+    // return reject(compose(equals(action.id), prop('id')), state)
 
-  // !!! UNTESTED - server needs upgrade
+  // !!! UNTESTED - server doesn't handle tasks yet
   case ADD_TASK:
     return map(list =>
         list.id === action.id
         ? list
         : assoc('input', '', list),
       state)
+    // old version :
     // return _.map(state, list => (
     //   list.id === parseInt(action.id) ?
     //     list : _.create(list, { input: '' })))
 
   case INPUT_TASK:
     return map(list =>
-      list.id === parseInt(action.id)
+      list.id === toInt(action.id)
       ? assoc('input', action.input, list)
       : list,
     state)
@@ -38,12 +41,17 @@ export default function lists(state = [], action) {
     //     _.create(list, { input: action.input }) : list))
 
   case GOT_LISTS:
-    return map(l => ({
-      ...l,
-      id: parseInt(l.id),
-      key: parseInt(l.id),
-      input: '' }),
-    action.lists)
+    const processList = pipe(
+      evolve({ id: toInt }), // ain't that poetry ?
+      assoc('input', ''))
+    return map(processList, action.lists)
+    // Ramda saves me 2 lines, and increases readability
+    // return map(l => ({
+    //   ...l,
+    //   id: parseInt(l.id),
+    //   key: parseInt(l.id),
+    //   input: '' }),
+    // action.lists)
 
   default:
     return state;
--- src/client/reducers/taskReducer.js
+++ src/client/reducers/taskReducer.js
@@ -1,26 +1,26 @@
 /**
  * Created by Antoine on 20/07/2016.
  */
-import _ from 'lodash'
+import { concat, map, reject } from 'ramda'
 import { ADD_TASK, DEL_TASK, GOT_TASKS } from '../actions/tasks'
 import { DEL_LIST } from '../actions/lists'
 
 export default function tasks(state = [], action) {
   switch (action.type) {
   case ADD_TASK:
-    return _.concat(state, action.task);
+    return concat(state, action.task);
 
   case DEL_TASK:
-    return _.reject(state, task => (
-     task.id === parseInt(action.id)));
+    return reject(task => task.id === parseInt(action.id),
+      state)
 
   case DEL_LIST:
-    return _.reject(state, task => (
-     task.listId === parseInt(action.id)));
+    return reject(task => task.listId === parseInt(action.id),
+      state);
 
   case GOT_TASKS:
-    return _.map(action.tasks, t => (
-      { ...t, key: t.id }));
+    return map(t => ({ ...t, key: t.id }),
+      action.tasks)
 
   default:
     return state;
